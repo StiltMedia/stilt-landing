809b17e4b90ae7648734797f952ab92aebf0183d
Mark Natera <marknatera@gmail.com>
2015-09-18 19:53:44 -0400

    Added phone number to contact form and link to main site on thanks page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915194500) do
+ActiveRecord::Schema.define(version: 20150918233752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20150915194500) do
     t.text     "message"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "phone"
   end
 
   create_table "goals", force: :cascade do |t|
