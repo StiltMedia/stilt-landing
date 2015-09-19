5ddbe14c67f57b9ec9a0ce8a042ddd0b6d90b749
Mark Natera <marknatera@gmail.com>
2015-09-19 17:14:55 -0400

    Changed email contact

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,7 +2,7 @@ class Mailer < ApplicationMailer
 
   def contact_form(contact)
     @contact = contact
-    mail :to  => ["mark@stiltmedia.com"], :from => "Stilt-Landing@stiltmedia.com", :subject => "Contact Form | Stilt Landing"
+    mail :to  => ["info@stiltmedia.com"], :from => "Stilt-Landing@stiltmedia.com", :subject => "Contact Form | Stilt Landing"
   end
 
 end
